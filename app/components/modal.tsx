84550321ee46ccc22814953262b396055d554958
Nghị <nghingothanhnghi@gmail.com>
2025-03-12 19:33:06 +0700

    update

--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -5,13 +5,16 @@ import ReactDOM from 'react-dom';
 interface ModalProps {
   isOpen: boolean;               // Whether the modal is open or not
   onClose: () => void;           // Function to close the modal
-  title: string;                 // Modal title
+  title?: string;                // Modal title
   content: ReactNode;            // Content inside the modal (can be anything like text, form, etc.)
   actions: ReactNode;            // Actions (buttons) that will be displayed in the footer
   size?: 'small' | 'medium' | 'large'; // Modal size (default is 'medium')
+  fullWidth?: boolean;
+  fullHeight?: boolean;
+  position?: 'center' | 'bottom';
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, content, actions, size = 'medium' }) => {
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, content, actions, size = 'medium', fullWidth = false, fullHeight = false, position = 'center'  }) => {
   // If the modal is not open, return null (don't render anything)
   if (!isOpen) return null;
 
@@ -22,11 +25,23 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, content, actions,
       large: 'w-4/5',
     };
 
+      // Positioning logic
+  const positionClasses = {
+    center: 'items-center', // Center vertically
+    bottom: 'items-end', // Stick to bottom with padding
+  };
+
   return ReactDOM.createPortal(
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className={`bg-white rounded-lg p-8 ${modalSizeClasses[size] || modalSizeClasses.medium} flex flex-col`}>
+    <div className={`fixed inset-0 z-50 flex justify-center ${positionClasses[position] || positionClasses.center} bg-black bg-opacity-50`}>
+      <div
+      className={`bg-white rounded-lg p-8 flex flex-col 
+        ${fullWidth ? 'w-full max-w-4/5' : modalSizeClasses[size] || modalSizeClasses.medium} 
+        ${fullHeight ? 'h-screen' : 'max-h-[90vh]'} 
+        overflow-y-auto`} 
+      // className={`bg-white rounded-lg p-8 ${modalSizeClasses[size] || modalSizeClasses.medium} flex flex-col`}
+      >
         <div className="flex justify-between items-center">
-          <h3 className="text-xl font-semibold">{title}</h3>
+          <h3 className="text-xl font-semibold">{title || '\u00A0'}</h3>
           <button className="text-gray-500" onClick={onClose}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
           </svg>
--- app/components/productFilter.tsx
+++ app/components/productFilter.tsx
@@ -1,7 +1,8 @@
 // components/ProductFilter.tsx
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import useResponsive from '~/hooks/useResponsive';
+import Modal from './modal';
 
 interface ProductFilterProps {
     onFilterChange: (newFilters: Record<string, any>) => void; // Declare the prop type
@@ -13,6 +14,14 @@ const ProductFilter: React.FC<ProductFilterProps> = ({ onFilterChange }) => {
     const [categories, setCategories] = useState('');
     const [priceRange, setPriceRange] = useState('');
     const [searchName, setSearchName] = useState('');
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const searchInputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (isModalOpen && searchInputRef.current) {
+            searchInputRef.current.focus();
+        }
+    }, [isModalOpen]);
 
     const handleCategoryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setCategories(event.target.value);
@@ -30,13 +39,13 @@ const ProductFilter: React.FC<ProductFilterProps> = ({ onFilterChange }) => {
         const filters = { categories, priceRange, name: searchName };
         console.log("Applying Filters:", filters);
         // Trigger the filter change when the user applies filters
-        onFilterChange({ categories, priceRange, name: searchName });
+        onFilterChange(filters);
+        setIsModalOpen(false);
     };
 
     return (
         <div className="bg-white p-6 gap-4 mx-auto mb-5">
-            <div className="flex flex-wrap gap-4 justify-center items-center">
-                {/* Search by Name */}
+            {/* <div className="flex flex-wrap gap-4 justify-center items-center">
                 <div>
                     <label className="block text-sm font-medium text-gray-600 mb-2">{t("input.searchByName.label")}</label>
                     <input
@@ -56,7 +65,6 @@ const ProductFilter: React.FC<ProductFilterProps> = ({ onFilterChange }) => {
                     >
                         <option value="">All</option>
                         <option value="Thực phẩm hữu cơ">Thực phẩm hữu cơ</option>
-                        {/* Add more categories here */}
                     </select>
                 </div>
 
@@ -85,7 +93,105 @@ const ProductFilter: React.FC<ProductFilterProps> = ({ onFilterChange }) => {
 
                     </button>
                 </div>
-            </div>
+            </div> */}
+            {isMobile ? (
+                <div className="flex items-center justify-between">
+                    <div className="w-full flex items-center bg-gray-100 rounded-lg p-2 pe-4 cursor-pointer" onClick={() => setIsModalOpen(true)}>
+                        <input
+                            type="text"
+                            value={searchName}
+                            readOnly
+                            placeholder={t("input.searchByName.placeholder")}
+                            className="h-10 rounded border-transparent text-sm flex-grow bg-transparent focus:outline-none"
+                        />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                        </svg>
+                    </div>
+                    <Modal
+                        isOpen={isModalOpen}
+                        onClose={() => setIsModalOpen(false)}
+                        fullWidth
+                        position="bottom"
+                        content={
+                            <div className="flex flex-col gap-4">
+                                <input
+                                    type="text"
+                                    ref={searchInputRef}
+                                    value={searchName}
+                                    onChange={handleSearchChange}
+                                    placeholder={t("input.searchByName.placeholder")}
+                                    className="h-10 rounded border-gray-300 text-sm w-full focus:outline-none"
+                                />
+                                <div>
+                                    <label className="block text-sm font-medium text-gray-600 mb-2">{t("select.category.label")}</label>
+                                    <select value={categories} onChange={handleCategoryChange} className="h-10 rounded border-gray-300 text-sm w-full">
+                                        <option value="">All</option>
+                                        <option value="Thực phẩm hữu cơ">Thực phẩm hữu cơ</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label className="block text-sm font-medium text-gray-600 mb-2">{t("select.priceRange.label")}</label>
+                                    <select value={priceRange} onChange={handlePriceRangeChange} 
+                                    className="h-10 rounded border-gray-300 text-sm w-full">
+                                        <option value="">All</option>
+                                        <option value="low">Low</option>
+                                        <option value="medium">Medium</option>
+                                        <option value="high">High</option>
+                                    </select>
+                                </div>
+                            </div>
+                        }
+                        actions={
+                            <button 
+                            onClick={handleApplyFilters} 
+                            className="bg-green-600 w-full text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-green-700 transition duration-300">
+                                {t("btn.search")}
+                            </button>
+                        }
+                    />
+                </div>
+            ) : (
+                <div className="flex flex-wrap gap-4 justify-center items-center">
+                    <div>
+                        <label className="block text-sm font-medium text-gray-600 mb-2">{t("input.searchByName.label")}</label>
+                        <input
+                            type="text"
+                            value={searchName}
+                            onChange={handleSearchChange}
+                            placeholder={t("input.searchByName.placeholder")}
+                            className="h-10 rounded border-gray-300 text-sm"
+                        />
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium text-gray-600 mb-2">{t("select.category.label")}</label>
+                        <select value={categories} onChange={handleCategoryChange} className="h-10 rounded border-gray-300 text-sm">
+                            <option value="">All</option>
+                            <option value="Thực phẩm hữu cơ">Thực phẩm hữu cơ</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium text-gray-600 mb-2">{t("select.priceRange.label")}</label>
+                        <select value={priceRange} onChange={handlePriceRangeChange} className="h-10 rounded border-gray-300 text-sm">
+                            <option value="">All</option>
+                            <option value="low">Low</option>
+                            <option value="medium">Medium</option>
+                            <option value="high">High</option>
+                        </select>
+                    </div>
+                    <div className="flex justify-between mt-6">
+                        <button
+                            onClick={handleApplyFilters}
+                            className="bg-green-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-green-700 hover:shadow-lg transition duration-300"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                            </svg>
+
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
