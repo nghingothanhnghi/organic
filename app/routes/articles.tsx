6ff0d3259b6eecea93a979e44727e256d39b1f8f
Nghị <nghingothanhnghi@gmail.com>
2025-03-15 14:36:13 +0700

    update

--- app/routes/articles.tsx
+++ app/routes/articles.tsx
@@ -11,7 +11,7 @@ const Articles = () => {
   const { t } = useTranslation();
   const breadcrumbItems = [
     { label: t("page_title.home"), path: "/" },
-    { label: t("page_title.articles"), path: "/articles" },
+    { label: t("page_title.post"), path: "/articles" },
   ];
   const dispatch = useAppDispatch();
   const { articles, loading, error, pagination, filters } = useAppSelector((state) => state.articles);
@@ -30,7 +30,9 @@ const Articles = () => {
     <div className="store-container">
       <Breadcrumb items={breadcrumbItems} />
       <div className="container mx-auto flex-column items-center justify-between py-4 px-6">
-      <h1 className="text-2xl font-bold mb-4">Bài viết</h1>
+      <h1 className="text-2xl font-bold mb-4">{t("page_title.post")}</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-10 gap-4 lg:gap-8 w-full">
+        <div className="col-span-1 lg:col-span-6 w-full lg:pe-10">
         <ArticleDisplay
           articles={articles}
           pagination={pagination}
@@ -41,6 +43,8 @@ const Articles = () => {
           error={error}
           viewMode="list"
         />
+        </div>
+      </div>
       </div>
     </div>
   );
