9023b1fdf1eab534efff2a4f10c21b7c1c130dfd
Nghị <nghingothanhnghi@gmail.com>
2025-03-16 11:33:11 +0700

    update

--- app/components/mobileMenuOffCanvas.tsx
+++ app/components/mobileMenuOffCanvas.tsx
@@ -21,11 +21,11 @@ const MobileMenuOffCanvas: React.FC<MobileMenuOffCanvasProps> = ({ isOpen, toggl
     <>
       {/* Mobile Navigation Off-canvas */}
       <nav
-        className={`fixed top-0 left-0 h-full w-96 bg-gray-100 shadow-lg transform transition-transform duration-300 ease-in-out ${isOpen ? "translate-x-0" : "-translate-x-full"
+        className={`fixed flex flex-col top-0 left-0 h-full w-96 bg-gray-100 shadow-lg transform transition-transform duration-300 ease-in-out ${isOpen ? "translate-x-0" : "-translate-x-full"
           } md:hidden z-50`}
       >
         {/* Header */}
-        <div className="flex items-center justify-between space-x-2 px-4 py-4 border-b border-gray-300">
+        <div className="flex items-center justify-between space-x-2 p-4">
           <button onClick={toggleMenu} className="text-gray-700 hover:text-gray-900 hover:bg-gray-200 transition-colors rounded-full p-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
@@ -33,27 +33,28 @@ const MobileMenuOffCanvas: React.FC<MobileMenuOffCanvasProps> = ({ isOpen, toggl
           </button>
           <UserProfileMobileMenu isOpen={isOpen} toggleMenu={toggleMenu} />
         </div>
-
-        {/* Menu Links */}
-        <ul className="flex flex-col items-start space-y-4 mt-4 px-4">
-          {loading ? (
-            <p className="text-gray-500">Loading...</p>
-          ) : error ? (
-            <p className="text-red-500">Error: {error}</p>
-          ) : (
-            menu.map((item) => (
-              <li key={item.id}>
-                <Link
-                  to={item.__component === "menu.menu-link" && item.url ? item.url : "/"}
-                  className="text-gray-700 hover:text-gray-900 transition-colors"
-                  onClick={toggleMenu} // Close menu on click
-                >
-                  {item.title}
-                </Link>
-              </li>
-            ))
-          )}
-        </ul>
+        <div className="flex-1 overflow-y-scroll p-3">
+          {/* Menu Links */}
+          <ul className="flex flex-col items-start space-y-4">
+            {loading ? (
+              <p className="text-gray-500">Loading...</p>
+            ) : error ? (
+              <p className="text-red-500">Error: {error}</p>
+            ) : (
+              menu.map((item) => (
+                <li key={item.id}>
+                  <Link
+                    to={item.__component === "menu.menu-link" && item.url ? item.url : "/"}
+                    className="text-gray-700 hover:text-gray-900 transition-colors"
+                    onClick={toggleMenu} // Close menu on click
+                  >
+                    {item.title}
+                  </Link>
+                </li>
+              ))
+            )}
+          </ul>
+        </div>
       </nav>
 
       {/* Backdrop */}
