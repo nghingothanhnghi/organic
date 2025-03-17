21088eb901388c308fa69643cfe97dd2185655d6
Nghị <nghingothanhnghi@gmail.com>
2025-03-17 11:51:54 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,6 @@ import tailwindcss from "tailwindcss";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// export default defineConfig({
-//   css: {
-//     postcss: {
-//       plugins: [tailwindcss, autoprefixer],
-//     },
-//   },
-//   plugins: [reactRouter(), tsconfigPaths()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:1337', // URL of your Strapi backend
-//         changeOrigin: true, // Ensures the origin header is updated to match the target URL
-//         secure: false, // Ignores SSL if Strapi uses self-signed certificates
-//       },
-//     },
-//   },
-// });
-
 export default defineConfig(({ mode }) => {
   // Load environment variables based on the mode (e.g., development or production)
   const env = loadEnv(mode, process.cwd());
