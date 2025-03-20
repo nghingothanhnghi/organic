dd9c44a5eb3bac1589713a392cd0f08c5bcaaddc
Nghị <nghingothanhnghi@gmail.com>
2025-03-20 21:57:05 +0700

    update

--- app/components/cartOffCanvas.tsx
+++ app/components/cartOffCanvas.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router";
 import { useAppSelector } from "~/hooks";
+import IconCartEmpty from "~/assets/empty.png";
 import CartList from "./cartList";
 import CartSummary from "./cartSummary";
 import ProceedToCheckoutButton from "./proceedToCheckoutButton";
@@ -52,9 +53,10 @@ const CartOffCanvas: React.FC<CartOffCanvasProps> = ({
         <div className="flex-1 overflow-y-scroll p-3">
           {isCartEmpty ? (
             <div className="flex flex-col items-center justify-center h-full text-center">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10">
+              <img src={IconCartEmpty} width={64} className="mb-1" />
+              {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
-              </svg>
+              </svg> */}
               <p className="text-gray-800 text-md font-semibold mb-4">{t("info.cart.message_01")}</p>
               <button
                 onClick={handleNavigateToStore}
