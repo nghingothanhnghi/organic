86958c7ff7007bd8d34b35c6db988205c19cbe22
Nghị <nghingothanhnghi@gmail.com>
2025-03-20 16:26:38 +0700

    update

--- app/routes/checkOut.tsx
+++ app/routes/checkOut.tsx
@@ -123,12 +123,19 @@ const CheckOut = () => {
                     {isMobile && (
                         <button
                             onClick={toggleClass}
-                            className="text-sm flex items-center gap-2 px-4 py-2 rounded-lg shadow-md"
+                            className="text-sm flex items-center px-3 py-2 rounded-lg hover:bg-gray-200 hover:shadow-md"
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
-                            </svg>
-                            <span className="font-semibold text-orange-700">{formatPrice(total)}</span>
+
+                            <span className="flex flex-col items-end">
+                                <span className="flex items-center gap-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+                                    </svg>
+                                    <span className="text-xs text-gray-500">{t("dataGrid.headerName.totalAmount")}</span>
+                                </span>
+                                <span className="text-xs font-semibold text-orange-700">{formatPrice(total)}</span>
+                            </span>
+
                             {/* {isActive ? "Hide Cart" : "Show Cart"} */}
                         </button>
                     )}
