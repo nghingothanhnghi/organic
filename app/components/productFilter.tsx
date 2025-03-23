0c867c49c849722d73a8531224f226b44103db5f
Nghị <nghingothanhnghi@gmail.com>
2025-03-23 16:06:59 +0700

    update

--- app/components/productFilter.tsx
+++ app/components/productFilter.tsx
@@ -45,55 +45,6 @@ const ProductFilter: React.FC<ProductFilterProps> = ({ onFilterChange }) => {
 
     return (
         <div className="bg-white p-6 gap-4 mx-auto mb-5">
-            {/* <div className="flex flex-wrap gap-4 justify-center items-center">
-                <div>
-                    <label className="block text-sm font-medium text-gray-600 mb-2">{t("input.searchByName.label")}</label>
-                    <input
-                        type="text"
-                        value={searchName}
-                        onChange={handleSearchChange}
-                        placeholder={t("input.searchByName.placeholder")}
-                        className="h-10 rounded border-gray-300 text-sm"
-                    />
-                </div>
-                <div>
-                    <label className="block text-sm font-medium text-gray-600 mb-2">{t("select.category.label")}</label>
-                    <select
-                        value={categories}
-                        onChange={handleCategoryChange}
-                        className="h-10 rounded border-gray-300 text-sm"
-                    >
-                        <option value="">All</option>
-                        <option value="Thực phẩm hữu cơ">Thực phẩm hữu cơ</option>
-                    </select>
-                </div>
-
-                <div>
-                    <label className="block text-sm font-medium text-gray-600 mb-2">{t("select.priceRange.label")}</label>
-                    <select
-                        value={priceRange}
-                        onChange={handlePriceRangeChange}
-                        className="h-10 rounded border-gray-300 text-sm"
-                    >
-                        <option value="">All</option>
-                        <option value="low">Low</option>
-                        <option value="medium">Medium</option>
-                        <option value="high">High</option>
-                    </select>
-                </div>
-
-                <div className="flex justify-between mt-6">
-                    <button
-                        onClick={handleApplyFilters}
-                        className="bg-green-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-green-700 hover:shadow-lg transition duration-300"
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                        </svg>
-
-                    </button>
-                </div>
-            </div> */}
             {isMobile ? (
                 <div className="flex items-center justify-between">
                     <div className="w-full flex items-center bg-gray-100 rounded-lg p-2 pe-4 cursor-pointer" onClick={() => setIsModalOpen(true)}>
