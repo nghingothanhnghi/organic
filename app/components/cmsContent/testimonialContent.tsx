5ce6e07ed42e4fb09e9e07a5fa7a1c755da822e5
Nghị <nghingothanhnghi@gmail.com>
2025-06-24 11:43:22 +0700

    update

--- app/components/cmsContent/testimonialContent.tsx
+++ app/components/cmsContent/testimonialContent.tsx
@@ -118,20 +118,19 @@ const TestimonialContent = () => {
                     <blockquote className="flex h-full flex-col justify-between bg-white rounded-lg  p-6 shadow-md sm:p-8 lg:p-12">
                       <div>
                         <div className="flex gap-0.5 text-green-500">
-                          {/* Generate stars dynamically */}
-                          {Array.from(
-                            { length: testimonial.rating },
-                            (_, i) => (
-                              <svg
+                          {Array.from({ length: 5 }, (_, i) => {
+                            const full = i < Math.floor(testimonial.rating);
+                            const half = i === Math.floor(testimonial.rating) && testimonial.rating % 1 >= 0.5;
+                            return (
+                              <StarIcon
                                 key={i}
-                                className="size-5 text-yellow-300"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                              >
-                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                              </svg>
-                            )
-                          )}
+                                filled={full}
+                                half={half}
+                                className="h-5 w-5"
+                                readOnly={true}
+                              />
+                            );
+                          })}
                         </div>
 
                         <div className="mt-4">
@@ -160,9 +159,8 @@ const TestimonialContent = () => {
         </div>
 
         <div
-          className={`mt-8 flex justify-center gap-4 lg:hidden ${
-            isMobile ? "lg:flex" : ""
-          }`}
+          className={`mt-8 flex justify-center gap-4 lg:hidden ${isMobile ? "lg:flex" : ""
+            }`}
         >
           {isMobile && (
             <>
--- app/components/inputSearchGlobal.tsx
+++ app/components/inputSearchGlobal.tsx
@@ -10,7 +10,7 @@ export const InputSearchGlobal: React.FC = () => {
     return (
         <form className="max-w-md mx-auto hidden lg:flex">
             <div className="relative">
-                <input type="search" className="block w-full py-2 ps-2 pe-10 text-sm text-gray-900 border border-gray-200 rounded-full bg-gray-200" placeholder="Search Posts, Products..." required />
+                <input type="search" className="block w-full py-2 ps-3 pe-10 text-sm text-gray-900 border border-gray-200 rounded-full bg-gray-200" placeholder="Search Posts, Products..." required />
                 <button type="submit" className="text-gray-700 absolute end-2.5 bottom-0.5 bg-transparent focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2 ">
                      <svg xmlns="http://www.w3.org/2000/svg" width={18} height={18} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
                 </button>
